0ee82b89f17d9fb2fd9add75c34606da47e0da66
Gojko Adzic <gojko@gojko.com>
2016-06-01 12:13:35 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+/*global module, Promise, require */
+var https = require('https');
+module.exports = function (callOptions) {
+	'use strict';
+	return new Promise(function (resolve, reject) {
+		var req = https.request(callOptions);
+
+		req.on('response', function (res) {
+			var dataChunks = [];
+			res.setEncoding('utf8');
+			res.on('data', function (chunk) {
+				dataChunks.push(chunk);
+			});
+			res.on('end', function () {
+				var response = {
+					headers: res.headers,
+					body: dataChunks.join(''),
+					statusCode: res.statusCode,
+					statusMessage: res.statusMessage
+				};
+				if (callOptions.resolveErrors || (response.statusCode > 199 && response.statusCode < 400)) {
+					resolve(response);
+				} else {
+					reject(response);
+				}
+			});
+		}).on('error', function (e) {
+			reject(e);
+		});
+		if (callOptions.body) {
+			req.write(callOptions.body);
+		}
+		req.end();
+	});
+};
